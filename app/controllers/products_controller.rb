e0c4788987af116fa612f66bb8bce79bbfd7ea5c
Santiago Almiron <sanalmiron@hotmail.com>
2021-03-26 16:39:45 -0300

    save changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,4 @@
 class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
-  def index
-     @products = Product.all
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'orders#new'
-  resources :order, only: [ :new ]
+  resources :order, only: [ :new ] do
+    resources :amount, only: [ :new ]
+  end
 end
