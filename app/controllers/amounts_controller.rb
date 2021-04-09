b3367f1ff8f7427f65edab229656e950a11d8630
Santiago Almiron <sanalmiron@hotmail.com>
2021-04-09 11:13:42 -0300

    added cart to mein page

--- app/controllers/amounts_controller.rb
+++ app/controllers/amounts_controller.rb
@@ -1,18 +1,26 @@
 class AmountsController < ApplicationController
+  after_action :amount_id_saver, only: :create
 
   def create
     @amount = Amount.new(amount_params)
-    if @amount.save
+    if @amount.amount >= 1 && @amount.save 
       redirect_to root_path
     else
-       redirect_to root_path
+      redirect_to root_path
     end
+    
   end
 
   private
 
   def amount_params
     params.require(:amount).permit(:amount, :product_id)
+  end
+
+  def amount_id_saver
+    session[:array] ||= []
+    session[:array].each {|id| session[:array].delete(id) if Amount.find_by_id(id) == nil}
+    session[:array] << @amount.id if @amount.id != nil
   end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @amount = Amount.new
+    @products_array = session[:array]
   end
 
 end
