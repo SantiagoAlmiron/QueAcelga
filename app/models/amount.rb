064ada262cb8d8a8ee5b3a451ca7819d08feb8fb
Santiago Almiron <sanalmiron@hotmail.com>
2021-04-10 17:28:29 -0300

    added send button to next page

--- app/models/amount.rb
+++ app/models/amount.rb
@@ -1,5 +1,4 @@
 class Amount < ApplicationRecord
   belongs_to :order, optional: true
-  # belongs_to :user, through: :order creo que no va a ser necesario llegar a user desde Amount
   belongs_to :product
 end
