fb63aafa7bc5a944313d4fd0e6999059a5d28f9d
Santiago Almiron <sanalmiron@hotmail.com>
2021-04-15 12:09:46 -0300

    saving partial changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,32 @@
 class OrdersController < ApplicationController
+before_action :amounts_array, only: [:new, :create]
+
 
   def new
     @order = Order.new
-    @amounts_id_array = session[:array]
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.user = current_user
+    if @order.save! 
+      redirect_to root_path
+      # total price 
+      # date 
+      # set amount id to the current order 
+    else
+      render new_order_path
+    end
   end
 
   private
 
+  def amounts_array
+    @amounts_id_array = session[:array]
+  end
+
   def order_params
-    params.require(:order).permit(:amount, :product_id)
+    params.require(:order).permit(:note)
   end
 
 end
